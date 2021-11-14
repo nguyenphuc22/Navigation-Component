b9e0b8507850927c5c029423a1f1d0d8f5cc3b04
nguyenphuc22 <59957741+nguyenphuc22@users.noreply.github.com>
2021-11-14 21:55:41 +0700

    Implement Basic

---
+++ NavigationBasic/app/src/main/java/com/example/navigationbasic/FirstFragment.kt
@@ -0,0 +1,37 @@
+package com.example.navigationbasic
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
+
+
+class FirstFragment : Fragment() {
+   lateinit var button : Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        var view = inflater.inflate(R.layout.fragment_first, container, false)
+        button = view.findViewById(R.id.button)
+        // Inflate the layout for this fragment
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        button.setOnClickListener {
+            Navigation.findNavController(button).navigate(R.id.action_firstFragment_to_twoFragment)
+        }
+    }
+
+}
