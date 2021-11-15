892c068fcbd71f75f7c72c7a65c1157bb404801c
nguyenphuc22 <59957741+nguyenphuc22@users.noreply.github.com>
2021-11-15 22:07:19 +0700

    update

--- SafeArgs/app/src/main/java/com/example/safeargs/FirstFragment.kt
+++ SafeArgs/app/src/main/java/com/example/safeargs/FirstFragment.kt
@@ -5,10 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
 
 
 class FirstFragment : Fragment() {
-
+    lateinit var button: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -18,9 +20,18 @@ class FirstFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        var view = inflater.inflate(R.layout.fragment_first, container, false)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        button = view.findViewById(R.id.button)
+        return view
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val action = FirstFragmentDirections.actionFirstFragmentToTwoFragment("Hi! I'm Phuc VR",20)
+        button.setOnClickListener {
+            Navigation.findNavController(button).navigate(action)
+        }
+    }
 
 }
--- SafeArgs/app/src/main/java/com/example/safeargs/TwoFragment.kt
+++ SafeArgs/app/src/main/java/com/example/safeargs/TwoFragment.kt
@@ -5,18 +5,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.navigation.NavArgs
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.navArgs
 
 
 class TwoFragment : Fragment() {
 
-
+    lateinit var textView: TextView
+    lateinit var button: Button
+    val twoFragmentArgs : TwoFragmentArgs by navArgs()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        var view = inflater.inflate(R.layout.fragment_two, container, false)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_two, container, false)
+        button = view.findViewById(R.id.button2)
+        textView = view.findViewById(R.id.textView);
+        return view
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        textView.text = twoFragmentArgs.message + twoFragmentArgs.number.toString()
+        button.setOnClickListener {
+            Navigation.findNavController(button).navigate(R.id.action_twoFragment_to_firstFragment)
+        }
+    }
 
 }
