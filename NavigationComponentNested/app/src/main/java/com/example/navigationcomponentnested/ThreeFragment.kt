2aa6e3eb74b92d4fa8536734d156ccf17fcda200
nguyenphuc22 <59957741+nguyenphuc22@users.noreply.github.com>
2021-11-30 15:40:42 +0700

    Create Component Nested

---
+++ NavigationComponentNested/app/src/main/java/com/example/navigationcomponentnested/ThreeFragment.kt
@@ -0,0 +1,32 @@
+package com.example.navigationcomponentnested
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
+
+class ThreeFragment : Fragment() {
+
+    lateinit var button: Button
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_three, container, false)
+        button = view.findViewById(R.id.btnToFour)
+        // Inflate the layout for this fragment
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        button.setOnClickListener {
+            Navigation.findNavController(button).navigate(R.id.action_threeFragment_to_fourFragment)
+        }
+    }
+
+}
---
+++ NavigationComponentNested/app/src/main/java/com/example/navigationcomponentnested/TwoFragment.kt
@@ -0,0 +1,21 @@
+package com.example.navigationcomponentnested
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
+
+class TwoFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_two, container, false)
+    }
+
+}
